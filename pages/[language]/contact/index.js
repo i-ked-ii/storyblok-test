f2824020aa9d99255e533c699fdebcbd69d8eed4
Suphawadee <ked.suphawadee@gmail.com>
2021-03-23 15:29:57 +0700

    fix layout props langauge

--- pages/[language]/contact/index.js
+++ pages/[language]/contact/index.js
@@ -1,20 +1,40 @@
+import React, { useState } from 'react';
 import Layout from '../../../components/Layout';
 import FeedbackForm from '../../../components/FeedbackForm';
 
-const Contact = () => {
+const Contact = (props) => {
   const services = {
     service_id: process.env.REACT_APP_EMAILJS_SERVICEID,
     template_id: process.env.REACT_APP_EMAILJS_TEMPLATEID,
     user_id: process.env.REACT_APP_EMAILJS_USERID,
   };
+  const [language] = useState(props.language);
 
   return (
-    <Layout>
+    <Layout language={language}>
       <div className="container mx-auto my-10 bg-white">
         <FeedbackForm env={services} title="Contact" />
       </div>
     </Layout>
   );
 };
+
+export const getStaticPaths = () => {
+  // return the story from Storyblok and whether preview mode is active
+  return {
+    paths: [{ params: { language: 'en' } }, { params: { language: 'de' } }],
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  let language = params.language || 'en';
+
+  return {
+    props: {
+      language,
+    },
+  };
+}
 
 export default Contact;
